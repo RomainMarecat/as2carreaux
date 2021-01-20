22c122b52ff7aca1bcd211a65497feb2332c5e09
Romain Marecat <romain.marecat@gmail.com>
2021-01-20 16:56:52 +0100

    init home

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,23 +13,4 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'as2carreaux'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('as2carreaux');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('as2carreaux app is running!');
-  });
 });
